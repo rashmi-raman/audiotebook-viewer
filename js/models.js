9d75b6e96a267d7680e430f6f0a3ef1eb85a9a46
Rashmi Raman <rr2779@columbia.edu>
2013-03-20 19:44:37 -0400

    addresses

--- js/models.js
+++ js/models.js
@@ -1,6 +1,22 @@
 $(function() {
 
-	window.Report = Backbone.Model.extend({
-    });
+	window.TastypieModel = Backbone.Model.extend({
+    base_url: function() {
+      var temp_url = Backbone.Model.prototype.url.call(this);
+      return (temp_url.charAt(temp_url.length - 1) == '/' ? temp_url : temp_url+'/');
+    },
+
+    url: function() {
+      return this.base_url();
+    }
+});
+
+
+    window.Report = window.TastypieModel.extend({
+
+    	defaults: {
+            address: ''
+        }
+	})
 
 });
